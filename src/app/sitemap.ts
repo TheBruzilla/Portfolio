4a1548e855b659a07f911bd656fb4bb5325d5d13
TheBruzilla <109081904+TheBruzilla@users.noreply.github.com>
2025-08-01 22:53:27 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,20 @@
+import { type MetadataRoute } from 'next';
 import { getPosts } from "@/utils/utils";
 import { baseURL, routes as routesConfig } from "@/resources";
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogs = getPosts(["src", "app", "blog", "posts"]).map((post) => ({
     url: `${baseURL}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
+    changefreq: 'weekly',
+    priority: 0.8,
   }));
 
   const works = getPosts(["src", "app", "work", "projects"]).map((post) => ({
     url: `${baseURL}/work/${post.slug}`,
     lastModified: post.metadata.publishedAt,
+    changefreq: 'monthly',
+    priority: 0.7,
   }));
 
   const activeRoutes = Object.keys(routesConfig).filter((route) => routesConfig[route as keyof typeof routesConfig]);
@@ -17,6 +22,8 @@ export default async function sitemap() {
   const routes = activeRoutes.map((route) => ({
     url: `${baseURL}${route !== "/" ? route : ""}`,
     lastModified: new Date().toISOString().split("T")[0],
+    changefreq: 'monthly',
+    priority: route === "/" ? 1.0 : 0.9,
   }));
 
   return [...routes, ...blogs, ...works];
