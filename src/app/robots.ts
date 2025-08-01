7a4320724332b5c79235a703c13ecc8452b30d82
TheBruzilla <109081904+TheBruzilla@users.noreply.github.com>
2025-08-01 22:48:27 +0530

    SEO site Management ✨

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,29 @@
-import { baseURL } from "@/resources";
+import { type MetadataRoute } from 'next';
 
-export default function robots() {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-      },
-    ],
-    sitemap: `${baseURL}/sitemap.xml`,
-  };
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = 'https://praneon.com';
+
+  return [
+    {
+      url: `${siteUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/gallery`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/work`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/blog`,
+      lastModified: new Date(),
+    },
+    // Add more dynamic routes if needed
+  ];
 }
